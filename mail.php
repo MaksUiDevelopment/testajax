6d9cba768eaba5601c2045ac69b4f921ce204d1a
MaksUiDevelopment <104228603+MaksUiDevelopment@users.noreply.github.com>
2022-12-20 23:02:06 +0300

    ajax
    1

---
+++ mail.php
@@ -0,0 +1,42 @@
+<?php
+echo '<pre>';
+print_r($_POST);
+echo '</pre>';
+
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$label = trim($_POST["label"]);
+
+$message = "Имя: $name\r\nТелефон: $phone\r\nФорма: $label";
+
+$to = "maksnazarov936@gmail.com";
+$title = "Новая заявка";
+
+$res = '';
+$error = false;
+
+if ( empty($name) ) {
+	$res .= '<div class="form-error form-error--name">Поле имя - не заполено</div>';
+	$error = true;
+}
+
+if ( empty($phone) ) {
+	$res .= '<div class="form-error form-error--phone">Поле телефон - не заполено</div>';
+	$error = true;
+} else {
+	$pattern = '/^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/';
+	if ( ! preg_match($pattern, $phone) ) {
+		$res .= '<div class="form-error form-error--phone">Поле телефон - не соответствует формату</div>';
+		$error = true;
+	}
+}
+
+if ( !$error ) {
+	if ( mail($to, $title, $message, "Content-type: text/plain; charset=\"utf-8\"") ) {
+		$res = '<div class="form-success">Сообщение отправлено, спасибо за заявку!</div>';
+	} else {
+		$res = '<div class="form-error">Ошибка при отправке</div>';
+	}
+}
+
+echo $res;
